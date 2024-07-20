f179ce4b6b34ddf3379f8c5433450efff7f4cf5b
Lonnickr <lonnickr@gmail.com>
2024-07-20 19:56:19 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 console.log("Harry Potter memory card game!")
+
+const section = document.querySelector("section");
+const playerLivesCount = document.querySelector("span");
+
+playerLivesCount.textContent = playerLives
