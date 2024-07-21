10ee902fef5d8f6f2d4071d16e6db07d0bfaf4b6
Lonnickr <lonnickr@gmail.com>
2024-07-20 20:47:06 -0400

    upadte2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 console.log("Harry Potter memory card game!")
 
 const section = document.querySelector("section");
-const playerLivesCount = document.querySelector("span");
+const moves = document.getElementById("moves-count");
+const timevalue = document.getElementById("time");
+const stopButton = document.getElementById("stop");
+const startButton = document.getElementById("start");
+const gameContainer = document.querySelector("game-container");
+const result = document.getElementById("result");
+const controls = document.querySelector("controls-container");
 
-playerLivesCount.textContent = playerLives
+let cards;
+let interval;
+let firstCard = false;
+let seconDcard = false;
+
+const items = [
+    { name: "fawkes1", image: "fawkes1.jpeg"},
+    { name: "fawkes2", image: "fawkes2.jpeg"},
+    { name: "gryffindor", image: "gryffindor.jpeg"},
+    { name: "hufflepuff", image: "hufflepuff.jpeg"},
+    { name: "ravenclaw", image: "ravenclaw.jpeg"},
+    { name: "slytherin", image: "slytherin.jpeg"}
+]
