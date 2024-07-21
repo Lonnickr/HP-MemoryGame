9462a678a855e208ae2cdec43a0b813cd2b8a4da
Lonnickr <lonnickr@gmail.com>
2024-07-21 15:13:46 -0400

    update

--- js/app.js
+++ js/app.js
@@ -15,16 +15,34 @@ let firstCard = false;
 let seconDcard = false;
 
 const getItems = () => [
-    { name: "fawkes1", image: "fawkes1.jpeg" },
-    { name: "fawkes2", image: "fawkes2.jpeg" },
-    { name: "gryffindor", image: "gryffindor.jpeg" },
-    { name: "hufflepuff", image: "hufflepuff.jpeg" },
-    { name: "ravenclaw", image: "ravenclaw.jpeg" },
-    { name: "slytherin", image: "slytherin.jpeg" }
+    { name: "fawkes1", image: "../images/fawkes\ 1.jpeg"},
+    { name: "fawkes2", image: "../images/fawkes\ 2.jpeg" },
+    { name: "gryffindor", image: "../images/gryffindor.jpeg" },
+    { name: "hufflepuff", image: "../images/hufflepuff.jpeg" },
+    { name: "ravenclaw", image: "../images/ravenclaw.jpeg" },
+    { name: "slytherin", image: "../images/slytherin.jpeg" }
 ]
 
 const randomize = () => {
     const items = getItems ();
-    console.log(items);
+    items.sort(() => Math.random() - 0.5);
+    return items; 
 }
-randomize ();
+
+const cardGenerator = () => {
+    const items = randomize();
+    items.forEach((items)=> {
+    const card = document.createElement("div");
+    const face = document.createElement("img");
+    const back = document.createElement("img");
+    card.classList = "card";
+    face.classList = "face";
+    back.classList = "back";
+face.src = items.image;
+
+    section.appendChild(card);
+    card.appendChild(face);
+    card.appendChild(back);
+    });   
+};
+cardGenerator()
